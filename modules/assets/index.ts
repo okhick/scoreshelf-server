a048b95229c1bf46ec832f9ea79434c49f349e76
Oliver Hickman <okhick@gmail.com>
2020-10-10 14:44:51 -0400

    full pdf2pic prototype in express

--- modules/assets/index.ts
+++ modules/assets/index.ts
@@ -3,13 +3,11 @@ import { AssetDB } from './controllers/asset-db';
 
 import PDFParser from "pdf2json";
 import { fromBase64 } from "pdf2pic";
-import { readFileSync } from "fs";
+import { readFileSync, mkdirSync, rmdirSync, fstat } from "fs";
 
 import { Application, Request, Response } from "express";
 import { AssetDataRequest, DeleteAssetRequest, UploadRequest, UploadResponse } from './@types';
-
-
-
+import { PDFJson } from 'pdf2json/typings';
 
 module.exports = function(app: Application) {
 
@@ -32,7 +30,6 @@ module.exports = function(app: Application) {
       const mongoRes = await assetDb.saveAssetData(upload);
       response[mongoRes.asset_name] = {_id: mongoRes._id};
     }
-    console.log(response)
     return res.json(response);
   });
   
@@ -65,9 +62,30 @@ module.exports = function(app: Application) {
   });
 
   app.get("/testpdfparse", async (req: Request, res: Response) => {
-    const PDF = readFileSync('/var/server/brickwall.pdf');
-    const pdfData = await getPdfJSON(PDF);
-    return res.json(pdfData);
+    const pageToConvertAsImage = 1;
+    const PDF = readFileSync('/var/server/Vue.js Cheat Sheet.pdf');
+    const pdfJSON = await getPdfJSON(PDF);
+    const pdfData = getPdfDimensions(pdfJSON, pageToConvertAsImage);
+
+    const imgDimensions = setImageDimensions(pdfData.ratio);
+
+    const imageOptions = {
+      density: 300,
+      saveFilename: pdfJSON.formImage.Agency,
+      savePath: "./temp",
+      format: "png",
+      width: imgDimensions.width,
+      height: imgDimensions.height,
+    };
+
+    // for some reason it was giving me greif about just a normal buffer
+    const base = PDF.toString('base64');
+    const convertToImage = fromBase64(base, imageOptions);
+
+    mkdirSync("./temp");
+    await convertToImage(pageToConvertAsImage);
+    res.sendFile(`/var/server/temp/${pdfJSON.formImage.Agency}.${pageToConvertAsImage}.png`);
+    res.on('finish', () => rmdirSync("./temp",{recursive:true}));
   })
   
   // TESTS
@@ -77,7 +95,7 @@ module.exports = function(app: Application) {
 
 }
 
-function getPdfJSON(pdf: Buffer) {
+function getPdfJSON(pdf: Buffer): Promise<PDFJson> {
   const pdfParser = new PDFParser();
   return new Promise((resolve, reject) => {
     // why do i have to assign these to use them in the listeners
@@ -85,7 +103,7 @@ function getPdfJSON(pdf: Buffer) {
     const rej = reject;
     
     // not sure why they made these listeners not just promises...
-    pdfParser.on("pdfParser_dataReady", (pdfData: any) => {
+    pdfParser.on("pdfParser_dataReady", (pdfData: PDFJson) => {
       resolve(pdfData);
     });
     pdfParser.on("pdfParser_dataError", (errData: any) => {
@@ -93,4 +111,30 @@ function getPdfJSON(pdf: Buffer) {
     });
     pdfParser.parseBuffer(pdf);
   });
+}
+
+function getPdfDimensions(pdfJSON: PDFJson, pageIndex: number): 
+  { width: number, height: number, ratio: number } {
+  // Page unit equals (96px/inch * 1inch / 4) = 24px/inch. See pdf2json/lib/pdfunit.js
+  const pixelsPerInch = 24;
+
+  const width = pdfJSON.formImage.Width * pixelsPerInch;
+  const height = pdfJSON.formImage.Pages[pageIndex].Height * pixelsPerInch;
+  const ratio = width / height;
+  return { width, height, ratio }
+}
+
+function setImageDimensions(ratio: number): 
+  { width: number, height: number } {
+  const longSide = 900;
+  let height = longSide;
+  let width = height * ratio;
+
+  // if this is true, it's landscape and we need scale to width = 900
+  if (ratio > 1) {
+    const mul = longSide / width;
+    width = width * mul;
+    height = height * mul;
+  }
+  return { width, height }
 }
